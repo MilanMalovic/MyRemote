52ea0194002b77111618247de1d3cf0a386a4bf5
Milan Malovic <milan.malovic@yahoo.com>
2018-01-15 12:30:54 +0100

    "Dodat pozdrav sa master grane!"

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,9 +3,11 @@ public class Pozdrav {
    
    public static void main(String[] args){
 	
-    system.out.println("Pozdrav iz prvog fajla")
+    system.out.println("Pozdrav iz prvog fajla");
 
- 
+
+	
+    system.out.println("Dodat pozdrav sa master grane");
 
 
  }
